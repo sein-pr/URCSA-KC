0b1e1f6460b81a1fee5ca238756e3036dcd416e9
Prince Sein <seinprince2@gmail.com>
2024-09-17 20:22:33 +0200

    new homepage

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,7 @@
-let currentIndex = 0;
-const images = document.querySelectorAll('.carousel-img img');
-const totalImages = images.length;
-const indicators = document.querySelectorAll('.indicator');
-
-function showNextImage() {
-    currentIndex = (currentIndex + 1) % totalImages;
-    updateCarousel();
-}
-
-function showPrevImage() {
-    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
-    updateCarousel();
-}
-
-function setCurrentImage(index) {
-    currentIndex = index;
-    updateCarousel();
-}
-
-function updateCarousel() {
-    const offset = -currentIndex * 100; // Move by 100% of the width of one image
-    document.querySelector('.carousel-img').style.transform = `translateX(${offset}%)`;
-    
-    // Update indicators
-    indicators.forEach((indicator, index) => {
-        indicator.classList.toggle('active', index === currentIndex);
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth'
     });
-}
-
-// Auto slide
-setInterval(showNextImage, 5000); // Change image every 5 seconds
-
+    calendar.render();
+});
